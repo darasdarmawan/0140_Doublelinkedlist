7ea7891cf1b3717340a2c5e3bf5590293567957f
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-05-04 11:44:41 +0700

    menambahkan kondisi if

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -82,5 +82,15 @@ bool search(int rollNo, Node** previous, Node** current)
 
 void deleteNode()
 {
+	Node* previous, * current; 
+	int rollNo;
 
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	cin >> rollNo; // step 3: get the roll number to be deleted
+
+	if (START == NULL)
+	{
+		cout << "List is empty" << endl;
+		return;
+	}
 }
